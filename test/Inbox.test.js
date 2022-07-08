34b1cc252bdda77ad88f5ce6d1ca516694f26dc0
MVRKKSK <miryala.kautilya2020@vitbhopal.ac.in>
2022-07-08 18:15:42 +0530

    fetched local test accounts

--- test/Inbox.test.js
+++ test/Inbox.test.js
@@ -1,5 +1,16 @@
 const ganache = require("ganache-cli")
 const Web3 = require("web3")
+const assert = require("assert")
+const { isTypedArray } = require("util/types")
 
 const web3 = new Web3(ganache.provider())
 
+let accounts;
+
+beforeEach(async() => {
+    accounts = await web3.eth.getAccounts()
+})
+
+describe("Inbox" , () => {
+    it('deploys a contract' , () => {console.log(accounts)})
+})

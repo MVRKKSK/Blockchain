188aaf5a7f3811cfe6a6609b90459a630f6f52cc
MVRKKSK <miryala.kautilya2020@vitbhopal.ac.in>
2022-07-11 12:39:51 +0530

    created bin folder for abi and bytecode

--- compile.js
+++ compile.js
@@ -1,29 +1,46 @@
 const path = require("path");
-const fs = require("fs");
+const fs = require("fs-extra");
 
 const solc = require("solc");
+
+const exportPath = path.resolve(__dirname , "bin")
+fs.removeSync(exportPath)
 
 const inboxPath = path.resolve(__dirname , "contracts" , "Inbox.sol");
 const source = fs.readFileSync(inboxPath , 'utf-8');
 
-
-/* console.log(solc.compile(source,1)) */
-
-var input = {
-    language: 'Solidity',
-    sources: {
-             'Inbox.sol': {
-              content: source
+const input = {
+   language: 'Solidity',
+   sources: {
+     'Inbox.sol': {
+       content: source,
+     },
+   },
+   settings: {
+     outputSelection: {
+       '*': {
+         '*': ['*']
+       }
+     }
    }
-},
-    settings: {
-              outputSelection: {
-              '*': {
-                 '*': ['*']
-                  }
-               }
-      }
-};
-var output = JSON.parse(solc.compile(JSON.stringify(input)));
-console.log(output)
+ };
+ 
+ //compile contract
+ try{
+  const output = JSON.parse(solc.compile(JSON.stringify(input)),1);
 
+  for (let contract in output.contracts["Inbox.sol"]) {
+    fs.outputJSONSync(
+      path.resolve(exportPath, "InboxABI.json"),
+      output.contracts["Inbox.sol"][contract].abi
+    );
+
+    fs.outputJSONSync(
+      path.resolve(exportPath, "InboxBytecode.json"),
+      output.contracts["Inbox.sol"][contract].evm.bytecode.object
+    );
+  }
+
+}catch(error){
+  console.log(error);
+}
--- test/Inbox.test.js
+++ test/Inbox.test.js
@@ -1,16 +1,23 @@
 const ganache = require("ganache-cli")
 const Web3 = require("web3")
 const assert = require("assert")
-const { isTypedArray } = require("util/types")
+const {interface , bytecode} = require("../compile")
 
 const web3 = new Web3(ganache.provider())
 
 let accounts;
+let inbox;
+console.log(interface)
+console.log(bytecode)
 
 beforeEach(async() => {
     accounts = await web3.eth.getAccounts()
+
+    inbox = await new web3.eth.Contract(JSON.parse(interface)).
+    deploy({data: bytecode , arguments: ["hi, there!"]}).
+    send({from: accounts[0]})
 })
 
 describe("Inbox" , () => {
-    it('deploys a contract' , () => {console.log(accounts)})
+    it('deploys a contract' , () => {console.log(inbox)})
 })

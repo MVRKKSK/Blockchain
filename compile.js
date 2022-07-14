e4a0e715b88b92781b6549507af0f387e222a51e
MVRKKSK <miryala.kautilya2020@vitbhopal.ac.in>
2022-07-14 14:07:19 +0530

    compilejs added and ready to deploy

--- compile.js
+++ compile.js
@@ -3,8 +3,8 @@ const fs = require("fs-extra");
 
 const solc = require("solc");
 
-const exportPath = path.resolve(__dirname , "bin")
-fs.removeSync(exportPath)
+// const exportPath = path.resolve(__dirname , "bin")
+// fs.removeSync(exportPath)
 
 const inboxPath = path.resolve(__dirname , "contracts" , "Inbox.sol");
 const source = fs.readFileSync(inboxPath , 'utf-8');
@@ -26,21 +26,14 @@ const input = {
  };
  
  //compile contract
- try{
-  const output = JSON.parse(solc.compile(JSON.stringify(input)),1);
 
-  for (let contract in output.contracts["Inbox.sol"]) {
-    fs.outputJSONSync(
-      path.resolve(exportPath, "InboxABI.json"),
-      output.contracts["Inbox.sol"][contract].abi
-    );
 
-    fs.outputJSONSync(
-      path.resolve(exportPath, "InboxBytecode.json"),
-      output.contracts["Inbox.sol"][contract].evm.bytecode.object
-    );
-  }
+ const output = JSON.parse(solc.compile(JSON.stringify(input)));
 
-}catch(error){
-  console.log(error);
-}
+ const interface = output.contracts["Inbox.sol"].Inbox.abi
+ const bytecode = output.contracts['Inbox.sol'].Inbox.evm.bytecode.object;
+ 
+ module.exports = {
+     interface,
+     bytecode,
+ };
--- test/Inbox.test.js
+++ test/Inbox.test.js
@@ -1,23 +1,42 @@
-const ganache = require("ganache-cli")
+const ganache = require("ganache")
 const Web3 = require("web3")
 const assert = require("assert")
-const {interface , bytecode} = require("../compile")
 
-const web3 = new Web3(ganache.provider())
+const provider = ganache.provider();
+const web3 = new Web3(provider);
+const {interface,bytecode} = require("../compile")
+
+
 
 let accounts;
 let inbox;
-console.log(interface)
-console.log(bytecode)
+
+
 
 beforeEach(async() => {
     accounts = await web3.eth.getAccounts()
 
-    inbox = await new web3.eth.Contract(JSON.parse(interface)).
+    inbox = await new web3.eth.Contract(interface).
     deploy({data: bytecode , arguments: ["hi, there!"]}).
-    send({from: accounts[0]})
+    send({from: accounts[0] , gas:1500000})
+
+    inbox.setProvider(provider);
 })
 
 describe("Inbox" , () => {
-    it('deploys a contract' , () => {console.log(inbox)})
+    it('deploys a contract' , () => {
+        assert.ok(inbox.options.address)
+    })
+
+    it('has a passing message' , async() => {
+        const message = await inbox.methods.message().call()
+        assert.equal(message , 'hi, there!')
+    })
+
+    it('set new message' , async() => {
+        await inbox.methods.setMessage('fuck off!').send({from: accounts[0]})
+        const message = await inbox.methods.message().call()
+        assert.equal(message , 'fuck off!')
+        
+    })
 })
